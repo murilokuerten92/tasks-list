89ec32d6cba197967f39a6052c4e77e10d1f4bce
Murilo Kuerten <murilo.kuerten@vialaserdepilacao.com.br>
2022-06-27 06:19:18 -0300

    feat: creating app header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 import logo from './logo.svg';
-import { Dashboard } from './pages/Dashboard';
+import { Dashboard } from '@/pages/Dashboard';
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <Dashboard />
