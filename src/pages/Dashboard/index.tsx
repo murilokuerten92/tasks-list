abd4085a3aad1eb21e584207243432aa7151c41a
Murilo Kuerten <murilo.kuerten@vialaserdepilacao.com.br>
2022-07-05 06:45:07 -0300

    feat: creating add task list

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, FormEvent } from "react";
 import * as S from "./styles";
 import logo from "@/assets/images/logo.png";
 import AddSVG from "@/assets/icons/add.svg";
@@ -6,10 +6,36 @@ import ReportSVG from "@/assets/icons/report.svg";
 import TrashSVG from "@/assets/icons/trash.svg";
 import CheckSVG from "@/assets/icons/check.svg";
 
+interface Tasks {
+  id: number;
+  name: string;
+  checked: boolean;
+}
+
 export const Dashboard = () => {
+  const [checked, setChecked] = useState(false);
+
+  const [tasks, setTasks] = useState<Tasks[]>([]);
+
   const [newTask, setNewTask] = useState("");
 
-  const [checked, setChecked] = useState(false);
+  function handleTaskCreate(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    console.log(newTask);
+
+    setTasks([...tasks, { id: Math.random(), name: newTask, checked: false }]);
+  }
+
+  console.log(tasks);
+
+  function handleTaskRemove(task: Tasks) {
+    const taskIndex = tasks.indexOf(task);
+
+    tasks.splice(taskIndex, 1);
+
+    setTasks([...tasks]);
+  }
 
   return (
     <S.Container>
@@ -18,23 +44,25 @@ export const Dashboard = () => {
       </header>
       <div className="main">
         <section className="main__aside">
-          <input
-            placeholder="Adicione uma nova tarefa"
-            value={newTask}
-            onChange={(e) => setNewTask(e.target.value)}
-            className="main__aside__input"
-          />
-          <button className="main__aside__button">
-            <h4 className="main__aside__h4">Criar</h4>
-            <img src={AddSVG} alt="add" />
-          </button>
+          <form onSubmit={handleTaskCreate}>
+            <input
+              placeholder="Adicione uma nova tarefa"
+              value={newTask}
+              onChange={(e) => setNewTask(e.target.value)}
+              className="main__aside__input"
+            />
+            <button type="submit" className="main__aside__button">
+              <h4 className="main__aside__h4">Criar</h4>
+              <img src={AddSVG} alt="add" />
+            </button>
+          </form>
         </section>
         <main className="main">
           <header className="main__container">
             <div className="main__tasks">
               <h6 className="main__tasks__text">Tarefas criadas</h6>
               <div className="main__tasks__amount">
-                <span>0</span>
+                <span>{tasks.length}</span>
               </div>
             </div>
 
@@ -47,21 +75,26 @@ export const Dashboard = () => {
           </header>
 
           <section className="main__list">
-            <div className="main__list__card">
-              <S.Radio  checked={checked} onClick={() => setChecked(!checked)}>
-               {checked && <img src={CheckSVG} alt="check" />} 
-              </S.Radio>
-              <div className="main__list__card__text">
-                <p>
-                  Integer urna interdum massa libero auctor neque turpis turpis
-                  semper. Duis vel sed fames integer. semper. Duis vel sed fames
-                  integer.
-                </p>
+            {tasks.map((task) => (
+              <div className="main__list__card" key={task.id}>
+                <div className="main__list__card__description"     onClick={() => setChecked(!checked)}>
+                  <S.Radio
+                    checked={checked}
+                  >
+                    {checked && <img src={CheckSVG} alt="check" />}
+                  </S.Radio>
+                  <div className="main__list__card__text">
+                    <S.Paragraph checked={checked}>{task.name}</S.Paragraph>
+                  </div>
+                </div>
+                <div
+                  className="main__list__card__trash"
+                  onClick={() => handleTaskRemove(task)}
+                >
+                  <img src={TrashSVG} alt="trash" />
+                </div>
               </div>
-              <div className="main__list__card__trash">
-                <img src={TrashSVG} alt="trash" />
-              </div>
-            </div>
+            ))}
 
             <img
               src={ReportSVG}
